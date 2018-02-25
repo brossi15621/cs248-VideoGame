da1c9409e9fc798ed3ee513b3a80aa47d8462d54
Austin Flores <acflores.stanford.edu>
2018-02-24 19:39:34 -0800

    Made a mirror script and adjusted monster speeds

---
+++ Finding Dawn/Assets/Scripts/MirrorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorController : MonoBehaviour {
+	private MeshRenderer myRenderer;
+
+	// Use this for initialization
+	void Start () {
+		myRenderer = gameObject.GetComponent<MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Mirror")) {
+			if (myRenderer.enabled) {
+				myRenderer.enabled = false;
+			} else {
+				myRenderer.enabled = true;
+			}
+		}
+	}
+}
