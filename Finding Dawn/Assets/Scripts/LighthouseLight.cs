0401a75cc5147327c9e3149279fdff4b482690f0
Austin Flores <acflores.stanford.edu>
2018-03-18 02:13:01 -0700

    Making Beach Map
    Started working on the beach map with the lighthouse moving safe zone

---
+++ Finding Dawn/Assets/Scripts/LighthouseLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LighthouseLight : MonoBehaviour {
+
+	Animation rotating;
+
+	// Use this for initialization
+	void Start () {
+		rotating = gameObject.GetComponent<Animation> ();
+		rotating ["rotate_light"].speed = .2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
