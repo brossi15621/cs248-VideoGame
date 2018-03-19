78dbb865694eaceb0b3e9b82f8f56e919ab7b286
Austin Flores <acflores.stanford.edu>
2018-03-19 00:00:56 -0700

    Merging all the files

---
+++ Finding Dawn/Assets/Scripts/DeadZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour {
+
+	private GameManagerScript manager;
+
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.Find ("Player").GetComponent<GameManagerScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(){
+		manager.dead = true;
+	}
+}
